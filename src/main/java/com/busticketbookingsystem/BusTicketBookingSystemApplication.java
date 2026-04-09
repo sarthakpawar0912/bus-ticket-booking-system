8684f520d9336bfc29e5bc291ff293aae84a6c55
sarthakpawar0912 <pawarsr06@gmail.com>
2026-04-09 11:10:16 +0530

    Initial Spring Boot project setup from sarthak

---
+++ src/main/java/com/busticketbookingsystem/BusTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.busticketbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketBookingSystemApplication.class, args);
+	}
+
+}
