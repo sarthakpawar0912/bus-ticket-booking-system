2f6b2d7639139e4290481b113a1b9387a9173e04
sarthakpawar0912 <pawarsr06@gmail.com>
2026-04-16 11:05:46 +0530

    " write test cases for thymeleaf "

--- src/main/java/com/busticketbookingsystem/BusTicketBookingSystemApplication.java
+++ src/main/java/com/busticketbookingsystem/BusTicketBookingSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // <-- Import this
 
 @SpringBootApplication
-@EnableScheduling // <-- Add this annotation here!
 public class BusTicketBookingSystemApplication {
 
 	public static void main(String[] args) {
