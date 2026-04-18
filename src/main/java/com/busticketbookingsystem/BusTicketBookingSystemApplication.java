b5593280f1aab3a0c8124987188c1cc4b6b45324
sarthakpawar0912 <pawarsr06@gmail.com>
2026-04-18 11:03:32 +0530

    " made some frontend changes"

--- src/main/java/com/busticketbookingsystem/BusTicketBookingSystemApplication.java
+++ src/main/java/com/busticketbookingsystem/BusTicketBookingSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // <-- Import this
 
 @SpringBootApplication
-@EnableScheduling // <-- Add this annotation here!
+@EnableScheduling
 public class BusTicketBookingSystemApplication {
 
 	public static void main(String[] args) {
